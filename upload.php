31e236f3f550e0661507ac99ad35f1cf8404853e
leHeix <43631341+leHeix@users.noreply.github.com>
2019-11-18 17:53:34 -0400

    global db connection in functions

--- upload.php
+++ upload.php
@@ -1,9 +1,7 @@
 <?php
-
 error_reporting(E_ALL);
 
 $config = include("config.php");
-
 $url = $config['url'];
 $db = $config['db'];
 $server = $config['server'];
@@ -14,8 +12,7 @@ $length = $config['randomstringlength'];
 $randomstring = $config['randomstring'];
 
 try {
-$conndata = "mysql:host=$server;dbname=$db";
-$connection = new PDO($conndata, $user, $pass);
+$connection = new PDO("mysql:host=$server;dbname=$db", $conndata, $user, $pass);
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
 	echo $e->getMessage();
@@ -29,66 +26,45 @@ function generateRandomString($length) {
     $randomString = '';
     for ($i = 0; $i < $length; $i++) {
         $randomString .= $characters[rand(0, $charactersLength - 1)];
-    $connection = new PDO("mysql:host=localhost;dbname=sharex", "sharexadmin", "tessa"); //TODO USE THIS WITH CONFIGS
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    function generateRandomString($length)
-    {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
-        }
-        return $randomString;
     }
+    return $randomString;
+}
 
-    function getuserinfo($connection, $db, $uid, $row)
-    {
-        $stmt = $connection->prepare("SELECT * FROM $db WHERE UID=$uid");
-        $stmt->execute();
-        $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-        $information = $result[$row];
-        return $information;
-    }
-      
 // I don't really know if this function works
-function getuserinfo($connection, $db, $uid, $row) {
+function getuserinfo($db, $uid, $row) {
+    global $connection;
     $query = $connection->prepare("SELECT * FROM ? WHERE UID = ?");
     $result = $query->execute(array($db, $uid));
     $rowresult = $query->fetchAll();
     return $rowresult;
 }
-    function TokenExists(string $token, $connection)
-    {
-        $stmt = $connection->prepare('SELECT COUNT(UserPassword) AS amount FROM sharex WHERE UserPassword = "' . $token . '"');
-        $stmt->execute();
-        $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-        return $result["amount"] > 0;
-    }
 
-
-
+function TokenExists(string $token) {
+    global $connection;
     $query = $connection->prepare('SELECT COUNT(UserPassword) FROM sharex WHERE UserPassword = "?"');
     $result = $query->execute(array($token));
     $row = $query->fetchAll();
     return $row > 0;
 }
-if (isset($_POST['token'])) {
-        if (TokenExists($_POST['token'], $connection)) {
-            if ($randomstring) {
-                $filename = generateRandomString($length); // TODO MOVE THIS SO I DONT NEED TO REPEAT CODE
-                $target = $_FILES["x"]["name"];
-                $extension = pathinfo($target, PATHINFO_EXTENSION);
 
-                if (move_uploaded_file($_FILES["x"]["tmp_name"], $dir . $filename . '.' . $extension)) {
-                    echo $url . $dir . $filename . '.' . $extension;
-                } else {
-                    echo "Possible permission error contact the server administrator.";
-                }
+
+if(isset($_POST['token'])) {
+    if(TokenExists($_POST['token'])) {
+        if($randomstring) {
+            $filename = generateRandomString($length); // TODO MOVE THIS SO I DONT NEED TO REPEAT CODE
+            $target = $_FILES["x"]["name"];
+            $extension = pathinfo($target, PATHINFO_EXTENSION);
+
+            if (move_uploaded_file($_FILES["x"]["tmp_name"], $dir.$filename.'.'.$extension)) {
+                echo $url . $dir . $filename . '.' . $extension;
             } else {
-                // TODO: get file name from sharex and dont use generateRandomString
+                echo "Possible permission error contact the server administrator.";
             }
+
+
+        } else {
+            // TODO: get file name from sharex and dont use generateRandomString
+        }
     } else {
         echo "Wrong Token\nContact server administrator";
     }
@@ -100,9 +76,3 @@ if (isset($_POST['token'])) {
 $connection = null;
 
 unset($config, $db, $server, $user, $pass);
-        echo "No post data received from client.";
-    }
-} catch(PDOException $e)
-{
-    echo "PDO Error: " . $e->getMessage();
-}
